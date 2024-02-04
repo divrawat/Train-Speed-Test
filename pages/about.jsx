33e4a1c9df8becd08d1fb0a0bc023e159df17b14
Divyanshu Rawat <divrawat2001@gmail.com>
2024-02-04 16:13:48 +0530

    vs0vfd5fvrrgvds

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Head from "next/head";
 import { DOMAIN, URL } from "@/config";
+import Link from "next/link";
 
 const About = () => {
 
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { DOMAIN, URL } from "@/config";
 import Head from "next/head";
+import Link from "next/link";
 
 const Contact = () => {
 
