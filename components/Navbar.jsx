8075a28f7dbba8286ad04e7e6b1cdf24377b3dcf
divrawat <divrawat2001@gmail>
2024-02-26 18:30:46 +0530

    sdf0vsdv5g5s4fsfv

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -26,12 +26,11 @@ export default function Navbar() {
                         <IoMdSpeedometer size={45} className="mr-3 ml-3  bg-gradient-to-r from-red-500 via-yellow-500 to-green-500" />
                         <span className=" text-lg font-bold tracking-wider text-[21px] md:text-[24px]"><Link href="/">Train Speed Test</Link></span>
                     </div>
-                    <span onClick={toggle} className="md:hidden text-[22px] font-extrabold mr-4">☰</span>
+                    <span onClick={() => toggle(menuRef)} className="md:hidden text-[22px] font-extrabold mr-4">☰</span>
                 </div>
 
                 <div className="md:pb-0 pr-3 md:mt-0 mt-4 md:bg-transparent ">
                     <ul id="menu" ref={menuRef} className="md:flex md:space-x-10 md:pb-0 pb-4 items-center font-bold  text-center leading-[3] hidden">
-                        {/* <li> <Link href="/">Home</Link></li> */}
                         <li> <Link href="/about">About</Link></li>
                         <li> <Link href="/contact">Contact</Link></li>
                         <li> <Link href="/disclaimer">Disclaimer</Link></li>
